69b4980ac5fecffbd58000affe1162051bcb93b9
WinterNox <121163909+WinterNox@users.noreply.github.com>
2023-11-25 20:05:12 +0530

    Add files via upload

--- Pseudo-Infinity/level.lua
+++ Pseudo-Infinity/level.lua
@@ -1,101 +1,99 @@
-local level_width, level_height = 3000fx, 3000fx
+local true_width, true_height = 1600fx, 900fx
+local level_width, level_height = 2fx * true_width + 100fx, 2fx * true_height + 100fx
+
+local range_x, range_y = true_width / 2fx, true_height / 2fx
+local center_x, center_y = level_width / 2fx, level_height / 2fx
+
 pewpew.set_level_size(level_width, level_height)
 
-local p1_index = 0
-local weapon_config = {frequency = pewpew.CannonFrequency.FREQ_10, cannon = pewpew.CannonType.DOUBLE}
-local player_one = pewpew.new_player_ship(1500fx, 1500fx, p1_index)
-pewpew.configure_player(p1_index, {shield = 999})
-pewpew.configure_player_ship_weapon(player_one, weapon_config)
+local player_index = 0
+local player_one = pewpew.new_player_ship(center_x, center_y, player_index)
+pewpew.configure_player(player_index, {shield = 64})  -- This level is just a showcase.
+pewpew.configure_player_ship_weapon(player_one, {
+  cannon = pewpew.CannonType.DOUBLE,
+  frequency = pewpew.CannonFrequency.FREQ_15
+})
 
---The code that loops the entities around the player
---It is self explanatory.
---ex, ey are entities' positions.
--- #entities is the number of entities
-function loop_entities()
-  local entities = pewpew.get_all_entities()
+function loop_player()
+  local px, py = pewpew.entity_get_position(player_one)
+  local previous_px, previous_py = px, py
 
-  for num=1,#entities do 
-    local ex, ey = pewpew.entity_get_position(entities[num])
-    
-    if fmath.to_int(ex) > fmath.to_int(player_x) + 600 then 
-      ex = player_x - 600fx
-    elseif fmath.to_int(ex) < fmath.to_int(player_x) - 600 then
-      ex = player_x + 600fx
-    end
-
-    if fmath.to_int(ey) > fmath.to_int(player_y) + 400 then
-      ey = player_y - 400fx
-    elseif fmath.to_int(ey) < fmath.to_int(player_y) - 400 then
-      ey = player_y + 400fx
-    end
-
-    pewpew.entity_set_position(entities[num], ex, ey)
+  if px > center_x + range_x then  -- Right edge
+    px = center_x - range_x
+  elseif px < center_x - range_x then  -- Left edge
+    px = center_x + range_x
   end
+
+  if py > center_y + range_y then  -- Top edge
+    py = center_y - range_y
+  elseif py < center_y - range_y then  -- Bottom edge
+    py = center_y + range_y
+  end
+
+  local dx, dy = px - previous_px, py - previous_py
+
+  if dx + dy ~= 0fx then  -- Needn't move the entities when the player does not loop
+    local entities = pewpew.get_all_entities()
+    for i = 1, #entities do
+      local entity = entities[i]
+      local ex, ey = pewpew.entity_get_position(entity)
+
+      ex = ex + dx
+      ey = ey + dy
+
+      pewpew.entity_set_position(entity, ex, ey)
+    end
+  end
+
+  -- Player's position is set to the desired location in the above for-loop itself.
 end
 
---The code that loops the player and displaces the entities to create the illusion
-function loop_player()
-  pewpew.entity_get_position(player_one)
-  local changes = 0
+function loop_entities()
+  local px, py = pewpew.entity_get_position(player_one)
 
-  if fmath.to_int(player_x) > fmath.to_int((level_width / 2fx) + 600fx) then
-    player_x = (level_width / 2fx) - 600fx
-    changes = changes + 1
-  elseif fmath.to_int(player_x) < (fmath.to_int(level_width) / 2) - 600 then
-    player_x = (level_width / 2fx) + 600fx
-    changes = changes + 1
-  end
+  local entities = pewpew.get_all_entities()
 
-  if fmath.to_int(player_y) > (fmath.to_int(level_height) / 2) + 400 then
-    player_y = (level_height / 2fx) - 400fx
-    changes = changes + 1
-  elseif fmath.to_int(player_y) < (fmath.to_int(level_height) / 2) - 400 then
-    player_y = (level_height / 2fx) + 400fx
-    changes = changes + 1
-  end
+  for i = 1, #entities do
+    local entity = entities[i]
+    local ex, ey = pewpew.entity_get_position(entity)
 
-  local cx, cy = pewpew.entity_get_position(player_one) --cx, cy are current positions of the player.
-  local dx = player_x - cx  --dx, dy are the difference between the new variable position and the position before the variables were changed.
-  local dy = player_y - cy
-
-  if changes > 0 then
-    local entities = pewpew.get_all_entities()
-
-    for j=1,#entities do
-      local ex, ey = pewpew.entity_get_position(entities[j])
-      pewpew.entity_set_position(entities[j], ex + dx, ey + dy)  --Offsets every entity (including the player)
+    if ex > px + range_x then  -- Right edge
+      ex = px - range_x
+    elseif ex < px - range_x then  -- Left edge
+      ex = px + range_x
     end
+
+    if ey > py + range_y then  -- Top edge
+      ey = py - range_y
+    elseif ey < py - range_y then  -- Bottom edge
+      ey = py + range_y
+    end
+
+    pewpew.entity_set_position(entity, ex, ey)
   end
 end
 
 local time = 0
-function level_tick()
+pewpew.add_update_callback(function()
   time = time + 1
   if pewpew.entity_get_is_alive(player_one) then
-    player_x, player_y = pewpew.entity_get_position(player_one)
-  end
+    local px, py = pewpew.entity_get_position(player_one)
 
-  local player1_score = pewpew.get_score_of_player(p1_index)  --To be removed for personal use
-  pewpew.increase_score_of_player(p1_index, player1_score * -1)  -- To be removed for personal use
+    -- (px - range_x, py - range_y) should be treated as (0fx, 0fx) in this system.
+    if time % 90 == 0 then
+      pewpew.new_asteroid(px + fmath.random_fixedpoint(-range_x, range_x), py + fmath.random_fixedpoint(-range_y, range_y))
+    end
 
-  if pewpew.get_entity_count(pewpew.EntityType.ASTEROID) < 1 then  --To be removed for personal use
-    pewpew.new_asteroid(fmath.random_fixedpoint(player_x - 600fx, player_x + 600fx), fmath.random_fixedpoint(player_y - 400fx, player_y + 400fx))
-  end
-  
-  if time % 300 == 0 then
-    local x = fmath.random_fixedpoint(player_x - 600fx, player_x + 600fx)
-    local y = fmath.random_fixedpoint(player_y - 400fx, player_y + 400fx)
-    local angle = fmath.random_fixedpoint(0fx, 629fx) / 100fx
-    pewpew.new_baf_blue(x, y, angle, 15fx, -1)
-  end
-  
-  local conf = pewpew.get_player_configuration(0)
-  if conf["has_lost"] == true then
-    pewpew.add_damage_to_player_ship(player_one, -1)
-  else
+    if time % 20 == 0 then
+      local angle = fmath.random_fixedpoint(0fx, fmath.tau())
+      pewpew.new_baf_blue(px + fmath.random_fixedpoint(-range_x, range_x), py + fmath.random_fixedpoint(-range_y, range_y), angle, 8fx, -1)
+    end
+
     loop_player()
+    loop_entities()
   end
-  loop_entities()
-end
 
-pewpew.add_update_callback(level_tick)
+  if pewpew.get_player_configuration(player_index)["has_lost"] then
+    pewpew.stop_game()
+  end
+end)
